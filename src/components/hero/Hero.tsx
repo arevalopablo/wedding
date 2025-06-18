4bb6ecc987bdfbaca0c0f0cd861c63ebf43abae5
Pablo <arevalopablo.pa@gmail.com>
2025-06-17 23:33:16 -0300

    'hero'

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -7,7 +7,7 @@ import foto2 from '../../assets/capri.jpg'
 
 const Hero = () => {
   return (
-    <Box sx={{background: {xs: `url(${foto})`, md: `url(${foto2})`}, backgroundRepeat: {xs: 'no-repeat'}, height: {xs: '430px', sm: '600px', md: '99vh'}, backgroundSize: {xs: 'cover', md: 'cover'}, backgroundPosition: {xs: 'contain', md: 'center'}, filter: {xs: 'grayscale(0)', md: 'grayscale(1)'}}}>
+    <Box sx={{background: {xs: `url(${foto})`, md: `url(${foto2})`}, backgroundRepeat: {xs: 'no-repeat'}, height: {xs: '430px', sm: '600px', md: '99vh'}, backgroundSize: {xs: 'cover', md: 'cover'}, backgroundPosition: {xs: 'center', md: 'center'}, filter: {xs: 'grayscale(0)', md: 'grayscale(1)'}}}>
     {/* <Box className={'hero-xs hero '} > */}
     
 
