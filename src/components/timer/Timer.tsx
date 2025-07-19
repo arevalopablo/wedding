ae8bfb66e91211f5b12d28659c014891771d572c
Pablo <arevalopablo.pa@gmail.com>
2025-07-18 23:35:15 -0300

    'update'

--- src/components/timer/Timer.tsx
+++ src/components/timer/Timer.tsx
@@ -11,11 +11,13 @@ const Timer = () => {
 
   return (
     <Box className={"timer-container"} sx={{height: {xs: '100vh', md: '100vh'}}}>
-      <Typography variant={"h1"} sx={{fontFamily: 'Great Vibes', fontSize: {xs: '60px', md: '120px'}, mb: '15px'}}>Maru & Pablo</Typography>
-      <Typography variant={"h1"} className="title" sx={{fontFamily: 'Great Vibes', fontSize: {xs: '45px', md: '90px'}}}>
+      <Typography variant={"h1"} sx={{fontFamily: 'Great Vibes', fontSize: {xs: '60px', md: '120px'}, color: 'rgb(185 155 96)', mt: '20px'}}>Maru</Typography>
+      <Typography variant={"h1"} sx={{fontFamily: 'Great Vibes', fontSize: {xs: '40px', md: '120px'}, color: 'rgb(185 155 96)'}}>&</Typography>
+      <Typography variant={"h1"} sx={{fontFamily: 'Great Vibes', fontSize: {xs: '60px', md: '120px'}, color: 'rgb(185 155 96)'}}>Pablo</Typography>
+      <Typography variant={"h1"} className="title" sx={{fontFamily: 'Great Vibes', fontSize: {xs: '40px', md: '90px'}}}>
         ¡¡¡ Nos Casamos !!!
       </Typography>
-      <Box className={"countdown-container"} mt={'30px'}>
+      <Box className={"countdown-container"}>
         <Grid
           container
           spacing={{ xs: 1, sm: 4, md: 6 }}
@@ -27,11 +29,11 @@ const Timer = () => {
             .map(([key, value], index) => (
               <Grid
                 className={"date-container"}
-                sx={{ width: { xs: "25%", sm: "150px", md: "170px" } }}
+                sx={{ width: { xs: "25%", sm: "150px", md: "170px", color: '#375849' } }}
                 p={'20px'}
                 key={index}
               >
-                <Typography variant="h1" sx={{ mb: "5px", fontFamily: 'Great Vibes', fontSize: '80px' }}>
+                <Typography variant="h1" sx={{ mb: "5px", fontFamily: 'Great Vibes', fontSize: {xs: '50px', md: '80px'} }}>
                   {value}
                 </Typography>
                 <Typography variant="h2" >{key}</Typography>
