2ff182d4f733e45208cd5a24ea11499651235082
Pablo <arevalopablo.pa@gmail.com>
2025-07-22 23:08:32 -0300

    'update'

--- src/components/timer/Timer.tsx
+++ src/components/timer/Timer.tsx
@@ -11,10 +11,10 @@ const Timer = () => {
 
   return (
     <Box className={"timer-container"} >
-      <Box width={{xs: '90%', sm: '80%',md: '80%', lg: '60%'}} border={'4px solid #333'} bgcolor={'rgba(255,255,255, .6)'} m={'0 auto'} p={{xs: '50px 0', md: '50px'}} boxShadow={'3px 3px 3px 20px rgba(255,255,255, .6)'}>
+      <Box width={{xs: '90%', sm: '80%',md: '80%', lg: '60%'}} bgcolor={'rgba(255,255,255, .6)'} m={'0 auto'} p={{xs: '50px 0', md: '50px'}} boxShadow={'3px 3px 3px 20px rgba(255,255,255, .6)'}>
         <Box display={"flex"} flexDirection={{xs: 'column', md: 'row'}} justifyContent={{md: 'center'}} alignItems={{md: 'center'}}>
         {['Maru', '&', 'Pablo'].map((name, index) => (
-          <Typography key={index} variant="h1" sx={{fontFamily: 'Great Vibes', fontSize: {xs: name === '&' ? '50px' : '70px', sm: name === '&' ? '60px' : '90px', md: name === '&' ? '60px' : '90px'}, m: {md: '0 10px'}, color: name === '&' ? '#afafaf' : ''}}>{name}</Typography>
+          <Typography key={index} variant="h1" sx={{fontFamily: 'Great Vibes', fontSize: {xs: name === '&' ? '50px' : '70px', sm: name === '&' ? '60px' : '90px', md: name === '&' ? '60px' : '90px'}, m: {md: '0 10px'}, color: name === '&' ? '#c1944a' : '#375849'}}>{name}</Typography>
         ))}
       </Box>
       <Typography variant={"h1"} className="title" sx={{fontFamily: 'Great Vibes', fontSize: {xs: '50px', sm: '60px', md: '60px'}, mt: {xs: '20px', sm: '10px', md: '10px'}}}>
